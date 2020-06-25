9057dcb1f7888a92b7b5cdc988b9c9760b13adbb
JuanGrajales <grajales.juanfe11@gmail.com>
2020-06-25 17:31:03 -0400

    add auto move and manual move for canvas rectangle

--- js/rect-auto-move.js
+++ js/rect-auto-move.js
@@ -3,10 +3,12 @@ let canvas = document.querySelector('canvas')
     canvas.height = 500
 let ctx = canvas.getContext('2d')
 let animationID
+let rectX = 30
 
 const animationLoop = () => {
     animationID = window.requestAnimationFrame(animationLoop)
-    ctx.fillRect(30, 200, 20, 20)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillRect(rectX += 5, 200, 20, 20)
 }
 
 animationLoop()
--- js/rect-manual-move.js
+++ js/rect-manual-move.js
@@ -3,10 +3,20 @@ let canvas = document.querySelector('canvas')
     canvas.height = 500
 let ctx = canvas.getContext('2d')
 let animationID
+let rectX = 30
 
 const animationLoop = () => {
     animationID = window.requestAnimationFrame(animationLoop)
-    ctx.fillRect(30, 200, 20, 20)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillRect(rectX, 200, 20, 20)
 }
 
+const move = (e) => {
+    console.log(`move`);
+    if(e.key === 'ArrowRight'){
+        rectX += 10
+    }
+}
+
+document.onkeydown = move
 animationLoop()
